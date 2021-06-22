aff000ee7a73f1a05d446a6dd9cd7c7835820797
Matías Irland <mirland@xmartlabs.com>
2021-06-22 13:06:06 -0300

    Handle onboarding command (#3)

--- src/com/xmartlabs/slackbot/Command.kt
+++ src/com/xmartlabs/slackbot/Command.kt
@@ -1,18 +1,6 @@
 package com.xmartlabs.slackbot
 
-enum class Command(val key: String) {
-    WIFI_PASS("wifi pass"),
-    TOGGL("toggl"),
-    RECYCLING("recycling"),
-    ANNIVERSARY("anniversary"),
-    CALENDAR_URL("calendar"),
-    SETUP_PROCESS("setup"),
-    LIGHTNING("lightning"),
-    BALA("bala"),
-    ;
-
-    companion object {
-        fun fromKey(key: String?) =
-            if (key.isNullOrBlank()) null else values().firstOrNull { key.contains(it.key, true) }
-    }
+class Command(vararg val keys: String, private val answerCallback: () -> String) {
+    val answer: String
+        get() = answerCallback()
 }
---
+++ src/com/xmartlabs/slackbot/CommandManager.kt
@@ -0,0 +1,153 @@
+package com.xmartlabs.slackbot
+
+@Suppress("MaxLineLength")
+object CommandManager {
+    private val default = Command() {
+        """
+                Hi :wave:! Check XL useful bots! :slack:
+                • *toggl* -> Where should I track this? :toggl_on:
+                • *wifi pass* -> Do you know Xmartlabs' office WIFI password? :signal_strength: :key:
+                • *recycling*-> Recycling help! :recycle:
+                • *anniversary* -> What happens in my anniversary/birthday? :tada: :birthday:
+                • *calendar urls?*-> Who is in TPO? When is the next lightning talk? :calendar:
+                • *setup process* -> Do you know what you have to do when you onboard to :xl: ?
+                • *lightning talks*-> WTF is a lightning talk :zap:?
+                
+                
+                This bot *WAS NOT* made by @bala
+                """.trimIndent()
+    }
+
+    val onboarding = Command("setup process", "onboarding") {
+        """
+
+                Welcome to Xmartlabs!:wave: :xl: We are very happy for having you onboard :muscle::muscle::muscle:  
+                 
+                Go to the Onboarding to know which are your next steps! 
+                https://www.notion.so/xmartlabs/Onboarding-c092b413380341948aabffa17bd85647 
+                    
+                Additionally, please: 
+                • Add a Profile picture to Slack & Bamboo :camera: :star: 
+                • Add calendar URLs :calendar:  -> https://www.notion.so/xmartlabs/Setup-Calendars-URLs-40a4c5506a03429dbdccea169646a8a3 
+                • Shot :absenta: (in next XL after)
+                
+                Regards @XlBot
+                """.trimIndent()
+    }
+
+    private val commands = listOf(
+        Command("anniversary") {
+            """
+                
+                *Anniversary* :tada: :birthday::
+                
+                • *Aniversarios y 3 meses en la empresa:* 
+                    • Cada mes contactamos a los que cumplen su aniversario y 3 meses en la empresa. 
+                    • Entre ustedes se organizan y eligen una forma de celebrar con todos (masitas, helado, pizza, etc) :masitas: :ice_cream: :pizza: :cookie:. Puede ser lo que ustedes quieran y cuándo quieran 
+                    • En los aniversarios la empresa les organiza una comida, regalitos y sorpresas por definir :eyes: 
+                    
+                • *Cumpleaños:* 
+                    • La empresa le regala al cumpleañero un desayuno :coffee: :croissant: :chocolate_bar: 
+                    • A fin de mes se hace un festejo para todos los cumpleañeros! La empresa compra tortas, bebidas y algo para picar. :birthday: :pizza:
+                """.trimIndent()
+        },
+        Command("calendar") {
+            """
+                
+                *Calendars* :calendar: :
+                    - https://www.notion.so/xmartlabs/Setup-Calendars-URLs-40a4c5506a03429dbdccea169646a8a3
+                """.trimIndent()
+        },
+        Command("lightning") {
+            """
+                
+                *Lightning talks* :flashlight:: 
+                The following is useful information about our lightning talks :zap:, 30 min talks where someone exposes something interesting he/she found.
+                
+                •  Lightning talks are added to the Eventos XL calendar :calendar: You will find the Zoom link meeting in there too :zoom:
+                •  Do you want to share something interesting with the team? Please follow this link :memo:
+                •  We record every lightning talk so don’t care if you are not able to join that day. You can find all the recorded lightning talks here :movie_camera:
+                •  Remember to track the talk in Toggl (Lightning talk -> Xmartlabs) :toggl_on:
+                """.trimIndent()
+        },
+        Command("recycling") {
+            """
+
+                *Recycling* :recycle::
+
+                Cosas que parecen reciclables pero no lo son 
+                • *Mezclados (Negro)*: 
+                    • Paquetes de yerba 
+                    • Paquetes de café
+                    • Boletos y tickets
+                • *Reciclables varios (verde):*
+                    • Caja de leche (Tetrapak)
+                    • Envoltorios de galletas (Todos)
+                    • Doypack (Envases de mayonesa, etc)
+                • *Envases plásticos (Amarillo):*
+                    • Botellas de plástico
+                    • Cualquier plástico que tenga el símbolo :recycle: 1 - PET o :recycle: 2 - PEAD
+                • *Papel y cartón (Azul):*
+                    • Cajas de te, edulcorante
+                    • Papel limpio y seco
+                • *Compostable (Rojo):*
+                    • Yerba
+                    • Café
+                    • Sobrecitos de té (con la bolsita y cuerda)
+                    • Cascaras de frutas y verduras
+                    • Restos de comida
+                    • Servilletas de papel usadas
+                
+                Recordá que todo lo reciclable tiene que estar limpio y seco :recycle:!
+                """.trimIndent()
+        },
+        Command("toggl") {
+            """
+                
+                *Toggl* :toggl_on: :
+                
+                • ¿Entrevistas? _Seleccion y entrevistas -> Xmartlabs_
+                • ¿Reviews? _Team Reviews -> Xmartlabs_
+                • ¿Lightning talks? _Lightning talk -> Xmartlabs_
+                • ¿1:1 con PM? _En tu proyecto._
+                • ¿1:1 con tu TL? _En tu equipo, Team X -> Xmartlabs._
+                • ¿Iniciativas? _Consulta a tu TL si la iniciativa en la que estás trabajando tiene un proyecto específico ya creado. Si es así, va en ese proyecto. En caso de que no, va dentro de tu equipo._
+                • ¿Code review o soporte a un compañero de otro proyecto? _Se trackea dentro de ese proyecto. En caso de que no lo veas en Toggl, comunicate con el PM de ese proyecto para que te agregue al team :)_
+                
+                En caso de tener alguna duda, consultá al equipo de operaciones o RRHH.
+              
+                """.trimIndent()
+        },
+        Command("wifi pass") {
+            """
+                *Wifi pass* :signal_strength: :key::
+                Internal: Xmartlabs33, Guests: xlinvitado
+            """.trimIndent()
+        },
+    ) + onboarding
+
+    init {
+        require(commands
+            .flatMap { it.keys.asList() }
+            .groupBy { it.lowercase() }
+            .values
+            .map { it.size }
+            .maxOrNull() == 1
+        ) {
+            "Duplicate commands are not allowed"
+        }
+    }
+
+    private fun String.sanitizeKey() = replace(" ", "_")
+        .lowercase()
+
+    fun provideCommand(userKey: String?): Command =
+        userKey?.let {
+            commands
+                .firstOrNull { command ->
+                    command.keys.any { commandKey ->
+                        userKey.sanitizeKey().contains(commandKey.sanitizeKey())
+                    }
+                }
+        } ?: default
+}
